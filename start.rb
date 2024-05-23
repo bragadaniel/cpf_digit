5576695650d0de4c584530ddbc98a7b7725df6f6
Daniel Braga <15815539+bragadaniel@users.noreply.github.com>
2024-05-22 21:43:23 -0300

    chore: update start.rb (#2)

--- start.rb
+++ start.rb
@@ -2,15 +2,9 @@ require './lib/generate_cpf'
 
 # Method that generates cpf without digits
 def build_cpf
-  cp = []
-  9.times.each do |c|
-    c = (0..9).to_a.sample
-    cp << c
-  end
-  cp
+  Array.new(9) { rand(0..9) }
 end
 
+cpf = CpfFake::CPF.new(build_cpf) # object
+cpf.build_digit # object.build
 
-cpf = CpfFake::CPF.new(build_cpf) #object
-
-cpf.build_digit #object.build
