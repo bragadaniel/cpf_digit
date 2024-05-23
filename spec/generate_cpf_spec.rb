94a3817cb34c6ceeda9c08e96dcdc2c4ace30391
Daniel Braga <15815539+bragadaniel@users.noreply.github.com>
2024-05-22 21:51:58 -0300

    chore: Update generate_cpf_spec.rb

--- spec/generate_cpf_spec.rb
+++ spec/generate_cpf_spec.rb
@@ -3,8 +3,20 @@ require './lib/generate_cpf'
 
 RSpec.describe CpfFake::CPF do
   describe 'CPF #digit test' do
-    it 'validate first digit'
+    let(:cpf_base) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
+    subject { described_class.new(cpf_base) }
 
-    it 'validate second digit'
+    it 'validates first digit' do
+      first_digit = subject.send(:calc_digit_I).last
+      expected_first_digit = 0 # calcule ou substitua pelo valor esperado com base no algoritmo
+      expect(first_digit).to eq(expected_first_digit)
+    end
+
+    it 'validates second digit' do
+      cpf_with_first_digit = subject.send(:calc_digit_I)
+      second_digit = subject.send(:calc_digit_J, cpf_with_first_digit).last
+      expected_second_digit = 9 # calcule ou substitua pelo valor esperado com base no algoritmo
+      expect(second_digit).to eq(expected_second_digit)
+    end
   end
 end
